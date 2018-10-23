451d975c96858aae146a411df87706a7ae8e4ec4
Emily Wolin <wolin.emily@gmail.com>
2018-10-23 10:17:21 -0600

    add control flow with argparse
    now you can re-start from the middle of the process w/o commenting stuff out!

--- addIRISPDFs.py
+++ addIRISPDFs.py
@@ -11,6 +11,8 @@ import pandas as pd
 from io import StringIO
 
 import json
+import argparse
+
 
 from noiseplot import setupPSDPlot
 from scipy import stats
@@ -22,6 +24,9 @@ import matplotlib.pyplot as plt
 # To do:
 # Read from config or supply as command line arg:
 #     - make list and map of nets/stns?
+#     - tradeoff: download pct_below_nlnm with value_gt > 0 the first time, and re-evaluate for various lifetime %s, 
+#                 *OR* request only for one value_gt, but would have to re-request if we decrease value_gt???
+# put some control flow in main() so we can skip straight to plotting if desired!! 
 ####################################
 def readConfig(jsonfile):
     '''Parse JSON configuration file, which should contain:
@@ -50,27 +55,44 @@ def readConfig(jsonfile):
 def readIRISfedcat(fedcatfile):
 # read file returned by fedcat, clean up headers, add columns holding start/end y-m-d for MUSTANG requests
 # specify dtypes explicitly so we know what we're working with later
-# note that we have to give column names including spaces etc. 
+# note that we also give column names including # and spaces 
 # to match format returned by IRIS fedcat request!
 # Then we clean them up using df.rename
     use_dtypes = {'#Network' : 'str',
                   ' Station ' : 'str',
                   ' Location ' : 'str',
                   ' Channel ' : 'str',
-                  ' Latitude ' : 'np.float64',
-                  ' Longitude ' : 'np.float64', 
-                  ' Elevation ' : 'np.float64',
-                  ' Depth ' : 'np.float64',
-                  ' Azimuth ' : 'np.float64',
-                  ' Dip ' : 'np.float64', 
+                  ' Latitude ' : np.float64,
+                  ' Longitude ' : np.float64, 
+                  ' Elevation ' : np.float64,
+                  ' Depth ' : np.float64,
+                  ' Azimuth ' : np.float64,
+                  ' Dip ' : np.float64, 
                   ' SensorDescription ' : 'str',
-                  ' Scale ' : 'np.float64',
-                  ' ScaleFreq ' : 'np.float64',
-                  ' ScaleUnits' : 'str',
+                  ' Scale ' : np.float64,
+                  ' ScaleFreq ' : np.float64,
+                  ' ScaleUnits ' : 'str',
                   ' SampleRate ' : 'int',
                   ' StartTime ' : 'str',
-                  ' EndTime ' : 'str'}
-    df = pd.read_csv(fedcatfile, sep='|', dtype={' Location ':'str'})
+                  ' EndTime ' : 'str',
+                  'Network' : 'str',
+                  'Station' : 'str',
+                  'Location' : 'str',
+                  'Channel' : 'str',
+                  'Latitude' : np.float64,
+                  'Longitude' : np.float64, 
+                  'Elevation' : np.float64,
+                  'Depth' : np.float64,
+                  'Azimuth' : np.float64,
+                  'Dip' : np.float64, 
+                  'SensorDescription' : 'str',
+                  'Scale' : np.float64,
+                  'ScaleFreq' : np.float64,
+                  'ScaleUnits' : 'str',
+                  'SampleRate' : 'int',
+                  'StartTime' : 'str',
+                  'EndTime' : 'str'}
+    df = pd.read_csv(fedcatfile, sep='|', dtype=use_dtypes)
     df.rename(columns=lambda x: x.strip(), inplace=True)
     df.rename(columns=lambda x: x.strip('#'), inplace=True)
 
@@ -112,29 +134,28 @@ def readIRISfedcat(fedcatfile):
 
 ####################################
 def getIRISfedcat(config, outname):
-# use requests to get fedcat file...
-# in bash: used
-# curl "http://service.iris.edu/irisws/fedcatalog/1/query?net=*&cha=HHZ,CHZ,DHZ,EHZ&format=text&includeoverlaps=true&nodata=404&datacenter=IRISDMC" > irisfedcat.txt
-# set NSLC target(s) and start/end dates in config file?
-    #reqstring = 'http://service.iris.edu/irisws/fedcatalog/1/query?net=*&cha=*HZ&format=text&includeoverlaps=true&nodata=404&datacenter=IRISDMC'
-    #reqstring = 'http://service.iris.edu/irisws/fedcatalog/1/query?net=II,IU,CU,IC,US&cha=*HZ&format=text&includeoverlaps=true&nodata=404&datacenter=IRISDMC'
-    #reqstring = 'http://service.iris.edu/irisws/fedcatalog/1/query?net=GS&cha=*HZ&format=text&includeoverlaps=true&nodata=404&datacenter=IRISDMC'
+    '''Get list of desired station-epochs from IRIS fedcat web service.
+       Use networks, stations, locations, and channels from config file to build fedcat request.
+       Then select only those where SampleRate > min_samp_rate in config file.'''
+
     reqstring = 'http://service.iris.edu/irisws/fedcatalog/1/query?net={0}&sta={1}&loc={2}&cha={3}&format=text&includeoverlaps=true&nodata=404&datacenter=IRISDMC'.format(config['networks'],config['stations'], config['locations'], config['channels'])
     print('requesting list of stations')
     res = requests.get(reqstring)
-    outfile = open(outname, 'w')
-    outfile.write(res.text)
-    outfile.close()
+    if res.status_code == 200:
+        print('List of stations received from IRIS fedcat')
+        outfile = open(outname, 'w')
+        outfile.write(res.text)
+        outfile.close()
+        print('Wrote initial list to {0}'.format(outname))
 
 # parse returned text just like we'd read a CSV
     df = readIRISfedcat(StringIO(res.text))
 
 # get only sample rates >= min_samp_rate, remember to reset indices!!
-# to do: set sample rate in config file?
-# to do: change this so we can specify 'selected' text file name!
     df = df[df.SampleRate >= config['min_samp_rate']].reset_index(drop=True)
     savecsv = 'irisfedcat_samprate_ge{0:d}.txt'.format(config['min_samp_rate'])
     df.to_csv(savecsv, sep='|', index=False)
+    print('Wrote list of stns with sample rate >= {0} to {1}'.format(config['min_samp_rate'],savecsv))
     return df
 ####################################    
 
@@ -167,6 +188,7 @@ def checkAboveLNM(df, config, getplots=False, outpbndir='PctBelowNLNM'):
         else:
             print('already requested pct_below_nlnm for {0}, will not re-request'.format(df.Target[i]))
             text = open(outpbnname, 'r').readlines()
+            print(outpbnname, df.Target[i])
             text = [ line.strip('\n') for line in text ][2:-1]
 
         # get pdf psd plots too for QC
@@ -331,7 +353,24 @@ def find_percentile(freq_u, db_u, newpdf_norm, perc, ax):
 # m___m
 ####################################
 def main():
-    args = sys.argv
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--doall", help="Restart from beginning: request stn-epochs, select by sample rate, check below NLNM, request PDFPSDs, sum, and plot", action='store_true')
+    parser.add_argument("--request_stns", help="Request list of stns-epochs defined in config file from IRIS fedcat", action='store_true', default=False)
+    parser.add_argument("--read_stns", help="Read list of stns from a pipe-separated file output by IRIS fedcat webservice")
+    parser.add_argument("--lnm_check", help="Check that PDF PSD values don't drop daily_perc_cutoff below Peterson NLNM for life_perc_cutoff % of lifetime", action='store_true', default=False)
+    parser.add_argument("--get_PDFs", help="request PDF PSDs from MUSTANG for each stn-epoch in dataframe", action='store_true', default=False)
+    parser.add_argument("--calc_PDF", help="Sum all PDF PSD files into one composite mega-PDF", action='store_true', default=False)
+    parser.add_argument("--plot_PDF", help="Plot composite PDF", action='store_true', default=False)
+
+    args = parser.parse_args()
+    if args.doall:
+        args.request_stns = True
+        args.lnm_check = True
+        args.get_PDFs = True
+        args.recalc_PDF = True
+        args.plot_PDF = True
+
+#    args = sys.argv
     if len(sys.argv) > 1:
         if sys.argv[1] == 'recalc':
             recalc = True
@@ -341,10 +380,6 @@ def main():
     config = readConfig('config.json')
 
 # Create output directories if they don't exist
-#    workdir = '/Users/ewolin/research/NewNewNoise/PermWithPython/' # to do : read this from config and/or default to '.'
-#    workdir = '/Users/ewolin/research/NewNewNoise/GSwithPython/' # to do : read this from config and/or default to '.'
-#    workdir = '.'
-
     workdir = config['workdir']
     outpdfdir = workdir+'/IndividualPDFs'
     outpercdir = workdir+'/Percentiles' # to do: supply as arg to checkAboveLNM, write percentile files to this dir
@@ -358,23 +393,30 @@ def main():
 
 
     outname = 'irisfedcat_initial.txt'
-    df = getIRISfedcat(config, outname)
-    
-    resume_lnm_check = False
-    if resume_lnm_check:
-    # find last target+start/end time that we checked
-        df_checked_already = pd.read_csv('used.txt', names=['Target', 'StartTime', 'EndTime'], sep=' ')
-        lastnscl = df_checked_already.iloc[-1].Target
-        laststart = df_checked_already.iloc[-1].StartTime
-        lastend = df_checked_already.iloc[-1].EndTime
-        i_restart = df[(df.Target == lastnscl) & (df.StartTime == laststart) & (df.EndTime == lastend)].index[0]
-        print('restarting from', i_restart)
-        df_restart = df.loc[i_restart:]
-        df_selected = checkAboveLNM(df_restart, config)
-        df_selected = df[:i_restart-1].append(df_selected)
+    if args.request_stns:
+        df = getIRISfedcat(config, outname)
     else:
+        df = readIRISfedcat(args.read_stns)
+
+# not sure how to work in resume check of LNM, comment out for now    
+#    resume_lnm_check = False
+#    if resume_lnm_check:
+#    # find last target+start/end time that we checked
+#        df_checked_already = pd.read_csv('used.txt', names=['Target', 'StartTime', 'EndTime'], sep=' ')
+#        lastnscl = df_checked_already.iloc[-1].Target
+#        laststart = df_checked_already.iloc[-1].StartTime
+#        lastend = df_checked_already.iloc[-1].EndTime
+#        i_restart = df[(df.Target == lastnscl) & (df.StartTime == laststart) & (df.EndTime == lastend)].index[0]
+#        print('restarting from', i_restart)
+#        df_restart = df.loc[i_restart:]
+#        df_selected = checkAboveLNM(df_restart, config)
+#        df_selected = df[:i_restart-1].append(df_selected)
+#    else:
+#        df_selected = checkAboveLNM(df, config)
+    if args.lnm_check:
         df_selected = checkAboveLNM(df, config)
-        
+    else:
+        df_selected = df.copy()
 
     
 # or if we've already run checkAboveLNM to produce a 'selected' file (or we've made it some other way): 
@@ -382,75 +424,70 @@ def main():
     #df_selected = pd.read_csv('irisfedcat_selected_gt200.txt', sep='|')
     #df_selected = readIRISfedcat('irisfedcat_selected_gt250.txt')
 
-    
-    requestPDFs(df_selected, outpdfdir)
+    if args.get_PDFs:
+        requestPDFs(df_selected, outpdfdir)
 
     pdffiles = listPDFFiles(df_selected, outpdfdir)
 
 # Find min/max freqs and dBs and sum PDFs    
     freq_u, db_u = findPDFBounds()
-    if recalc:
+    if args.calc_PDF:
         pdf = calcMegaPDF(freq_u, db_u, pdffiles)
     else:
         pdf = np.load('megapdf.npy')
 
+    if args.plot_PDF:
+# Plot PDF
+        cmap = pqlx
+        fig, ax = setupPSDPlot()
     
-    
-    # Plot PDF
-    cmap = pqlx
-    fig, ax = setupPSDPlot()
-    
-    newpdf_norm = np.zeros(shape=pdf.shape, dtype=np.float_)
+        newpdf_norm = np.zeros(shape=pdf.shape, dtype=np.float_)
 # normalize PDF since MUSTANG returns hit counts not %
-    for i in range(len(freq_u)):
-    #    print(freq_u[i], np.sum(pdf[i,:]))
-        if np.sum(pdf[i,:]) > 0:
-            newpdf_norm[i,:] = pdf[i,:]/np.sum(pdf[i,:])
-        else:
-            newpdf_norm[i,:] = pdf[i,:]*1e-10 # np.nan*pdf[i,:]
+        for i in range(len(freq_u)):
+            if np.sum(pdf[i,:]) > 0:
+                newpdf_norm[i,:] = pdf[i,:]/np.sum(pdf[i,:])
+            else:
+                newpdf_norm[i,:] = pdf[i,:]*1e-10 # np.nan*pdf[i,:]
     
-    im = ax.pcolormesh(1./freq_u, db_u, newpdf_norm.T, cmap=cmap, vmax=.30)
-    #ax.set_ylim(-367, 278)
+        im = ax.pcolormesh(1./freq_u, db_u, newpdf_norm.T, cmap=cmap, vmax=.30)
     
 # Plot PDF and save    
-    ax.plot([0.01, 0.3], [-137, -162], 'k--', label='200 sps noise model')
-    ax.plot([0.01, 0.731139], [-137,-168.536389686], 'c:', lw=5, label='200 sps noise model')
-    ax.plot([0.01, 1], [-137,-170.849624626], 'y:', lw=2, label='200 sps noise model')
-    dlogperiod = np.log10(0.3) - np.log10(0.01)
-    ddb = -137 - -162
-    y = -137 + ddb/dlogperiod * (np.log10(0.01)-np.log10(0.73))
-    #print(y)
-    #ax.plot(1, y, 'ko')
-
-    ax.plot([0.01, 0.1], [-91, -91], 'r--', label='GS high noise model?')
+        ax.plot([0.01, 0.3], [-137, -162], 'k--', label='200 sps noise model')
+        ax.plot([0.01, 0.731139], [-137,-168.536389686], 'c:', lw=5, label='200 sps noise model')
+        ax.plot([0.01, 1], [-137,-170.849624626], 'y:', lw=2, label='200 sps noise model')
+        dlogperiod = np.log10(0.3) - np.log10(0.01)
+        ddb = -137 - -162
+        y = -137 + ddb/dlogperiod * (np.log10(0.01)-np.log10(0.73))
+        #print(y)
+        #ax.plot(1, y, 'ko')
     
-    ax.grid()
+        ax.plot([0.01, 0.1], [-91, -91], 'r--', label='GS high noise model?')
     
-    find_percentile(freq_u, db_u, newpdf_norm, 0.01, ax)
-    y_full = find_percentile(freq_u, db_u, newpdf_norm, 0.02, ax)
-    iwhere, = np.where((freq_u >= 3)&(freq_u<=40))
-    x = freq_u[iwhere]
-    y = y_full[iwhere]
-    find_percentile(freq_u, db_u, newpdf_norm, 0.1, ax)
-    find_percentile(freq_u, db_u, newpdf_norm, 0.5, ax)
-    find_percentile(freq_u, db_u, newpdf_norm, 0.9, ax)
-    #find_percentile(newpdf_norm, 1.0)
+        ax.grid()
+    
+        find_percentile(freq_u, db_u, newpdf_norm, 0.01, ax)
+        y_full = find_percentile(freq_u, db_u, newpdf_norm, 0.02, ax)
+        iwhere, = np.where((freq_u >= 3)&(freq_u<=40))
+        x = freq_u[iwhere]
+        y = y_full[iwhere]
+        find_percentile(freq_u, db_u, newpdf_norm, 0.1, ax)
+        find_percentile(freq_u, db_u, newpdf_norm, 0.5, ax)
+        find_percentile(freq_u, db_u, newpdf_norm, 0.9, ax)
+        #find_percentile(newpdf_norm, 1.0)
 
 # need to get db, log(f or T) out of percentiles and do a linear fit
-    print(x)
-    x_log = np.log10(1./x)
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x_log,y)
-    y_new = x_log*slope+intercept
-    plt.plot(1./x, y_new, lw=5, color='pink')
-    print(min(1./x), max(1./x), min(y), max(y))
-    plt.plot(1./x, np.ones(len(x))*-180, lw=10, color='purple')
-   # for i,x in enumerate(x):
-   #     print(x[i], x[i]*slope+intercept)
+        print(x)
+        x_log = np.log10(1./x)
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x_log,y)
+        y_new = x_log*slope+intercept
+        plt.plot(1./x, y_new, lw=5, color='blue')
+        print(min(1./x), max(1./x), min(y), max(y))
+        plt.plot(1./x, np.ones(len(x))*-180, lw=10, color='purple')
     
-    ax.legend(ncol=3, loc='lower center', fontsize='small')
-    ax.set_xlim(0.005,10)
-    #ax.set_xlim(0.01,10)
-    fig.savefig('meh.png')
+        ax.legend(ncol=3, loc='lower center', fontsize='small')
+        ax.set_xlim(0.005,10)
+        #ax.set_xlim(0.01,10)
+        fig.savefig('meh.png')
 
 if __name__ == "__main__":
     main()
